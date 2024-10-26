5efb3ca6b82cdf907c0d8f7e4cb491b6f7cbb600
GitStudentSem <69849493+GitStudentSem@users.noreply.github.com>
2024-10-26 12:00:15 +0300

    lesson 139: delete current user

--- controllers/userController.js
+++ controllers/userController.js
@@ -52,6 +52,12 @@ exports.createUser = (req, res) => {
 		.json({ status: "error", message: "This route is not yet defined" });
 };
 
+exports.deleteMe = catchAsync(async (req, res, next) => {
+	await User.findByIdAndUpdate(req.user.id, { active: false });
+
+	res.status(204).json({ status: "success", data: null });
+});
+
 exports.getUser = (req, res) => {
 	res
 		.status(500)
