dbd61b36ca6948d974e93612e14a4c9d2eda0ba2
GitStudentSem <69849493+GitStudentSem@users.noreply.github.com>
2024-10-26 13:39:55 +0300

    lesson 143: set security HTTP headers with helmet

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const rateLimit = require("express-rate-limit");
-
+const helmet = require("helmet");
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
 const tourRouter = require("./routes/tourRoutes");
@@ -10,10 +10,15 @@ const userRouter = require("./routes/userRoutes");
 const app = express();
 
 // GLOBAL MIDDLEWARES
+// Set security HTTP headers
+app.use(helmet());
+
+// Development logging
 if (process.env.NODE_ENV === "development") {
 	app.use(morgan("dev"));
 }
 
+// Limit requests from same IP
 const limiter = rateLimit({
 	max: 100,
 	windowMs: 60 * 60 * 1000,
@@ -21,9 +26,13 @@ const limiter = rateLimit({
 });
 app.use("/api", limiter);
 
-app.use(express.json());
+// body parser, reading date from the body into req.body
+app.use(express.json({ limit: "10kb" }));
+
+//  Serving static files
 app.use(express.static(`${__dirname}/public`));
 
+// Test midleware
 app.use((req, res, next) => {
 	req.requestTime = new Date().toISOString();
 	next();
