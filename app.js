7c020a40092c836941d8c8af08b57609e0ed5aa0
GitStudentSem <69849493+GitStudentSem@users.noreply.github.com>
2024-10-26 13:53:39 +0300

    lesson 144: Data sanitization

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const morgan = require("morgan");
 const rateLimit = require("express-rate-limit");
 const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
 const tourRouter = require("./routes/tourRoutes");
@@ -28,6 +31,12 @@ app.use("/api", limiter);
 
 // body parser, reading date from the body into req.body
 app.use(express.json({ limit: "10kb" }));
+
+// Data sanatization against NoSQUL query injection
+app.use(mongoSanitize());
+
+//  Data sanatozation against XSS
+app.use(xss());
 
 //  Serving static files
 app.use(express.static(`${__dirname}/public`));
